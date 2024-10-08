5f1f9a994146d34bb9937086e63e8ea20a00c9d7
User Andrey <hommander3002@gmail.com>
2024-10-09 01:01:02 +0300

    спринт 6. начало

---
+++ src/Tests/HistoryListTest.java
@@ -0,0 +1,55 @@
+package tests;
+import controllers.InMemoryHistoryManager;
+import controllers.InMemoryTaskManager;
+import model.Task;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HistoryListTest {
+    @Test
+    public void checkAddFunction(){
+        Task task1 = new Task("sf", "sfdf");
+        Task task2 = new Task("aaaa", "ffff");
+        Task task3 = new Task("theiii", "dggg");
+
+        InMemoryTaskManager taskManger = new InMemoryTaskManager();
+        InMemoryHistoryManager history = new InMemoryHistoryManager();
+        List<Task> result = new ArrayList<>();
+        taskManger.addTask(task1);
+        taskManger.addTask(task2);
+        taskManger.addTask(task3);
+
+        taskManger.getTaskById(task1.getId());
+        taskManger.getTaskById(task2.getId());
+
+        result.add(task1);
+        result.add(task2);
+
+        assertEquals(taskManger.getHistory(), result);
+    }
+    @Test
+    public void checkRemoveFuction(){
+        Task task1 = new Task("sf", "sfdf");
+        Task task2 = new Task("aaaa", "ffff");
+        Task task3 = new Task("theiii", "dggg");
+
+        InMemoryTaskManager taskManger = new InMemoryTaskManager();
+        InMemoryHistoryManager history = new InMemoryHistoryManager();
+        List<Task> result = new ArrayList<>();
+        taskManger.addTask(task1);
+        taskManger.addTask(task2);
+        taskManger.addTask(task3);
+
+        taskManger.getTaskById(task1.getId());
+        taskManger.getTaskById(task2.getId());
+
+        result.add(task2);
+
+        taskManger.deleteTask(task1.getId());
+        assertEquals(taskManger.getHistory(), result);
+    }
+}
