1bad91eeffa9549f5f61f6e0c6953b919c8597e6
User Andrey <hommander3002@gmail.com>
2024-10-28 21:35:07 +0300

    #16 исправлние ошибок code style.

--- src/controllers/Managers.java
+++ src/controllers/Managers.java
@@ -1,13 +1,13 @@
 package controllers;
 
-import model.Task;
 
 public class Managers {
 
-    public static ITaskManager getDefault(){
+    public static ITaskManager getDefault() {
         return new InMemoryTaskManager();
     }
-    public static HistoryManager getDefaultHistory(){
+
+    public static HistoryManager getDefaultHistory()    {
         return new InMemoryHistoryManager();
     }
 }
