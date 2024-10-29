60e5faffb2b7813a565ce8b71db3aeec479280c8
User Andrey <hommander3002@gmail.com>
2024-10-29 21:04:32 +0300

    #26 codystyle ошибки

--- src/test/TestSubTask.java
+++ src/test/TestSubTask.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestSubTask {
