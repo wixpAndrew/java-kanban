561855ec5d5c7ca51849139e6ba15650e7c858f3
User Andrey <hommander3002@gmail.com>
2025-02-19 23:33:21 +0300

    4

--- src/main/java/task/Epic.java
+++ src/main/java/task/Epic.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 public class Epic extends Task {
     private final List<Subtask> subList = new ArrayList<>();
