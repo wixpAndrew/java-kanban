8b43b588363bd60d185c4a807afa71bcbfdd632e
User Andrey <hommander3002@gmail.com>
2025-02-20 22:12:18 +0300

    14

--- src/main/java/server/UtilHelper.java
+++ src/main/java/server/UtilHelper.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
 
 public class UtilHelper {
 
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSS");
 
     public static Gson getGsonBuilder() {
         Gson gson = new GsonBuilder()
